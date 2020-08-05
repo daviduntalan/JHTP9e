0fdb0827603234a281f87b28c47628bc4a14ef5f
'daviduntalan' <'daviduntalan@yahoo.com'>
2020-08-06 07:08:05 +0800

    Successfully called Stored Procedure using CallableStatement.

--- student_assignments/IQ18_HowTo_CallStoredProc.java
+++ student_assignments/IQ18_HowTo_CallStoredProc.java
@@ -11,33 +11,6 @@ import java.sql.CallableStatement;
 
 /**
  * Guide to this Code are coming from: https://youtu.be/NJAGYU6tQ9w
- * 
- * Things you need...
- * 
- * Existing database
- * - SQL Server
- * 
- * Jar file (sqljdbc.jar)
- * - libraries you need to connect to the DB
- * - driver
- * - download here: 
- * https://go.microsoft.com/fwlink/?linkid=852460 mssql-jdbc-6.2.2.jre8.jar 
- * https://go.microsoft.com/fwlink/?linkid=2063159 for JRE 8, 11
- * https://go.microsoft.com/fwlink/?linkid=2099962 for JRE 8, 11, 12
- * https://docs.microsoft.com/en-us/sql/connect/jdbc/microsoft-jdbc-driver-for-sql-server
- * https://www.youtube.com/redirect?v=nly_2b9QeO4&event=video_description&redir_token=QUFFLUhqbnRHV01CdFZ2UVktdlNrYzZHQ0RRUmhFLXFKQXxBQ3Jtc0tuTkEtM0tCM3FoVXFqRVROelJrT0dlUHZYX1VmZVA3MVdobmtlalVrQ0c4UFlVYWs0WFNWVkNkd0Rrb3pTYTFMcXhLUzkwWXlIQTduZ2NYV25OLU5RZHhldU54YW1SMDVKeGVYa3F6cUZhb1JURFpkWQ%3D%3D&q=http%3A%2F%2Fwww.java2s.com%2FCode%2FJar%2Fs%2FDownloadsqljdbc420jar.htm
- * 
- * Steps:
- * 1. connect to DB using JDBC URL
- * 2. create statement object
- * 3. invoke statement.executeQuery on query you're passing to the DB
- * 4. fetch the required data using the ResultSet
- * 5. close DB resources
- * 6. run the application
- * 
- * Change password in SQL Server:
- * - https://www.youtube.com/watch?v=TDonCJIQv7w
- * 
  * YT Channel: https://www.youtube.com/channel/UCM5ldu3IonPjffpYSnSYYJA/videos?view=0&sort=da&flow=grid
  * @author David
  */
@@ -50,7 +23,8 @@ public class IQ18_HowTo_CallStoredProc {
         String port = "64990"; // from SqlServer Config Mgr./NetProtocol for SQLExpress/IP Addresses/TCP Dynamic Ports
         String user = "sa"; // provide your own username and
         String password = "admin1234"; // password here, thank you.
-        String database = "MyCompany"; // table: Employee (employee_id, first_name, last_name, gender, position, department_id, salary)
+        String database = "MyCompany"; // table: Employee (employee_id, 
+        // first_name, last_name, gender, position, department_id, salary)
         
         Connection conn = null;
         CallableStatement cs = null;
@@ -66,16 +40,18 @@ public class IQ18_HowTo_CallStoredProc {
         try {            
             conn = ds.getConnection();                        
             cs = conn.prepareCall("{call Employee_getById(?)}");
-            cs.setInt("@EmpId", 2008); // you can use index column instead
+            cs.setInt("@EmpId", 2009); // you can use index column instead
             rs = cs.executeQuery();
 
-            System.out.printf("%-6s %-10s %-10s %6s %-20s %9s\n", 
+            System.out.printf("%-6s %-10s %-10s %-6s %-20s %-10s %9s\n", 
                     "Emp ID", "First", "Last", "Gender", 
-                    "Position", "Salary");            
+                    "Position", "Department", "Salary");
             while (rs.next()) {                
-                System.out.printf("%6d %-10s %-10s %6s %-20s %9.2f\n", 
+                System.out.printf("%6d %-10s %-10s %-6s %-20s %-10s %9.2f\n", 
                     rs.getInt(1), rs.getString(2), rs.getString(3), 
-                    rs.getString(4), rs.getString(5), rs.getDouble(7));            
+                    rs.getString(4), rs.getString(5), rs.getString(6), 
+                    rs.getDouble(7)
+                );            
             }
         } catch (SQLException ex) {
             Logger.getLogger(IQ18_HowTo_CallStoredProc.class.getName()).log(Level.SEVERE, null, ex);
